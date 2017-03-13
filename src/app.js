9948bc514eea1a5495f65ea642dbaf1d52fc8ebb
Ramsha Mushtaq <ramsha.a.mushtaq@gmail.com>
2017-03-13 14:52:45 -0700

    Create a route and make a GET request

--- src/app.js
+++ src/app.js
@@ -4,8 +4,16 @@ var express = require('express');
 
 var app = express();
 
+var port = 3001;
+
 app.get('/', function(req, res) {
-  res.send('Request received');
+  res.send('<h1> Express JS </h1>');
 });
 
-app.listen(3001);
+app.get('/blog', function(req, res) {
+  res.send('route added');
+})
+
+app.listen(port, function() {
+  console.log('listening on port: ', port);
+});
