b1c44f0ef92ff0a32e0441b6b0f44ab6f67c76a1
Ramsha Mushtaq <ramsha.a.mushtaq@gmail.com>
2017-03-13 15:00:48 -0700

    Set up a '/blog' route

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
 'use strict';
 
 var express = require('express');
+var posts = require('./mock/posts.json');
 
 var app = express();
+var port = 3001;
 
 app.get('/', function(req, res){
 	res.send("<h1>I Love Treehouse!</h1>");
 });
 
-app.listen(3000, function(){
-	console.log("The frontend server is running on port 3000!")
+app.get('/blog', function(req, res) {
+  res.send(posts);
+});
+
+app.listen(port, function(){
+	console.log('The frontend server is running on port: ', port);
 });
